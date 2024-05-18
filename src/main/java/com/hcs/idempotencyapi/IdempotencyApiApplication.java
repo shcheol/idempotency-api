0a8448596bde49e33b31535814f25603a76620bc
shcheol <kian1523@naver.com>
2024-05-18 16:33:03 +0900

    fix test, encoding

---
+++ src/main/java/com/hcs/idempotencyapi/IdempotencyApiApplication.java
@@ -0,0 +1,13 @@
+package com.hcs.idempotencyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class IdempotencyApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdempotencyApiApplication.class, args);
+	}
+
+}
