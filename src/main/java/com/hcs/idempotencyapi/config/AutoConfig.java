08eaad2c06725942a1c844a4af04c24a57fa8898
shcheol <kian1523@naver.com>
2024-05-19 13:12:45 +0900

    라이브러리의 빈들이 컴포넌트 스캔의 대상이 되도록 설정

---
+++ src/main/java/com/hcs/idempotencyapi/config/AutoConfig.java
@@ -0,0 +1,9 @@
+package com.hcs.idempotencyapi.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.hcs.idempotencyapi")
+public class AutoConfig {
+}
